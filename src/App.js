fb582083153bd9c0451849cb039b9fe2c2991fb2
victoramos1 <ramos_victor@outlook.com>
2023-09-07 19:58:49 -0300

    Atualização de currículo

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
           <div id="divTexto">
             <h5>Desenvolvedor Full-Stack</h5>
             <h1>Olá, eu sou <span style={{ color: '#03fc96', fontSize: '100px' }}>Victor</span></h1>
-            <a href="https://drive.google.com/file/d/1BOxh0izWf2sjdF7wlsrst1jrF8ESLmUd/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button id="btn">Ver currículo</button></a>
+            <a href="https://drive.google.com/file/d/1AUs3EXOAGo19D5rDpkOHjsEh7WInq-1S/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button id="btn">Ver currículo</button></a>
           </div>
         </Slide>
         <Jump >
